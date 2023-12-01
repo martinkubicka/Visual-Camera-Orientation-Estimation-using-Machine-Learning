b9256529137cba5f4e91b21a91e15aac2082ac59
martinkubicka <martinkubicka22@gmail.com>
2023-12-01 14:29:28 +0100

    update model

--- src/CameraPoseSCNN/model.py
+++ src/CameraPoseSCNN/model.py
@@ -1,15 +1,13 @@
-import argparse
+from sphere_cnn import SphereConv2D, SphereMaxPool2D
 import torch
 from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import numpy as np
-from sphere_cnn import SphereConv2D, SphereMaxPool2D
 from PIL import Image
 import os
 import csv
-
 from sklearn.model_selection import train_test_split
 
 class CustomDataset(Dataset):
@@ -47,6 +45,13 @@ class CustomDataset(Dataset):
         panorama_image_path, picture_image_path, csv_data = self.data[idx]
         panorama_image = Image.open(panorama_image_path)
         picture_image = Image.open(picture_image_path)
+        
+        panorama_image = panorama_image.convert('L')
+        picture_image = picture_image.convert('L')
+        
+        new_size = (panorama_image.width // 10, panorama_image.height // 10)
+        panorama_image = panorama_image.resize(new_size, Image.ANTIALIAS)
+        picture_image = picture_image.resize(new_size, Image.ANTIALIAS)
 
         if self.transform:
             panorama_image = self.transform(panorama_image)
@@ -63,37 +68,31 @@ class MergedModel(nn.Module):
         self.sphere_pool1 = SphereMaxPool2D(stride=2)
         self.sphere_conv2 = SphereConv2D(32, 64, stride=1)
         self.sphere_pool2 = SphereMaxPool2D(stride=2)
-
+        
         # second scnn
         self.sphere_conv3 = SphereConv2D(1, 32, stride=1)
         self.sphere_pool3 = SphereMaxPool2D(stride=2)
         self.sphere_conv4 = SphereConv2D(32, 64, stride=1)
         self.sphere_pool4 = SphereMaxPool2D(stride=2)
-
-        # merging layer
-        self.fc_merge = nn.Linear(64 * 13 * 13 * 2, 256)
-        self.fc = nn.Linear(256, 10)
+        
+        self.fc = nn.Linear(2 * 64 * 51 * 103, 3)
 
     def forward(self, x1, x2, transform_data):
         # first scnn
         x1 = F.relu(self.sphere_pool1(self.sphere_conv1(x1)))
         x1 = F.relu(self.sphere_pool2(self.sphere_conv2(x1)))
-        x1 = x1.view(-1, 64 * 13 * 13)
-
+        x1 = x1.view(-1, 64 * 51 * 103)
+        
         # second scnn
         x2 = F.relu(self.sphere_pool3(self.sphere_conv3(x2)))
         x2 = F.relu(self.sphere_pool4(self.sphere_conv4(x2)))
-        x2 = x2.view(-1, 64 * 13 * 13)
-
-        # merge
+        x2 = x2.view(-1, 64 * 51 * 103)
+        
         x = torch.cat((x1, x2), dim=1)
-        x = F.relu(self.fc_merge(x))
-
-        x = torch.cat((x, transform_data), dim=1)
-
+        
         x = self.fc(x)
+        
         return x
-
 
 def train(model, device, train_loader, optimizer, epoch):
     model.train()
@@ -101,10 +100,12 @@ def train(model, device, train_loader, optimizer, epoch):
         data1, data2, transform_data = data1.to(device), data2.to(device), transform_data.to(device)
         optimizer.zero_grad()
         output = model(data1, data2, transform_data)
+        _, transform_data = transform_data.max(dim=1) # TODO ints
+        
         loss = F.cross_entropy(output, transform_data)
         loss.backward()
         optimizer.step()
-        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f'.format(
+        print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
             epoch, batch_idx * len(data1), len(train_loader.dataset),
             100. * batch_idx / len(train_loader), loss.item()))
 
@@ -115,7 +116,12 @@ def test(model, device, test_loader):
     with torch.no_grad():
         for data1, data2, transform_data in test_loader:
             data1, data2, transform_data = data1.to(device), data2.to(device), transform_data.to(device)
-            output = model(data1, data2, transform_data)
+            output = model(data1, data2, transform_data) # TODO ints
+            
+            print(transform_data)
+            print(output)
+            
+            _, transform_data = transform_data.max(dim=1)
             test_loss += F.cross_entropy(output, transform_data).item()
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(transform_data.view_as(pred)).sum().item()
@@ -133,15 +139,15 @@ def main():
     device = torch.device("cpu")
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-    data_dir = "../preprocessing/dataImagesFromPanoramas/"
+    data_dir = "/kaggle/input/dataimagesfrompanoramas/dataImagesFromPanoramas/" # TODO CHANGE ME
 
     # train set
     train_dataset = CustomDataset(data_dir, transform=transform, train=True)
-    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True, **kwargs)
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, **kwargs)
 
     # test set
     test_dataset = CustomDataset(data_dir, transform=transform, train=False)
-    test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False, **kwargs)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, **kwargs)
 
     model = MergedModel().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
@@ -153,3 +159,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
